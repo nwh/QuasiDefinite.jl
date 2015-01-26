c10f67fc0f0ddc5c8074ef61b01a7eef038a7b13
Nick Henderson <nwh@stanford.edu>
2015-01-25 16:51:01 -0800

    initial work for building in julia

---
+++ deps/build.jl
@@ -0,0 +1,19 @@
+using BinDeps
+
+@BinDeps.setup
+
+libqdel = library_dependency("libqdel")
+
+depsdir = BinDeps.depsdir(libqdel)
+qdeldir = joinpath(depsdir,"src","libqdel")
+
+show(qdeldir)
+
+provides(SimpleBuild,
+         (@build_steps begin
+             ChangeDirectory(qdeldir)
+             `make`
+         end),
+         libqdel)
+
+@BinDeps.install [:libqdel => :libqdel]
--- src/QuasiDefinite.jl
+++ src/QuasiDefinite.jl
@@ -2,4 +2,9 @@ module QuasiDefinite
 
 # package code goes here
 
+# load the libqdel
+include("../deps/deps.jl")
+
+show("Hello from QuasiDefinite.")
+
 end # module
