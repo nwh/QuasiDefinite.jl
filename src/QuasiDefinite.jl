42d7f993d4ee8681fae48a41fd01361921b239e7
Nick Henderson <nwh@stanford.edu>
2015-02-02 15:36:46 -0800

    basic building blocks are in place now

--- src/QuasiDefinite.jl
+++ src/QuasiDefinite.jl
@@ -6,17 +6,33 @@ module QuasiDefinite
 import Base.blasfunc
 import Base.LinAlg: BlasReal, BlasComplex, BlasFloat, BlasChar, BlasInt,
     blas_int, DimensionMismatch, chkstride1, chksquare
-import Base.LinAlg.LAPACK: @chkuplo, @assertargsok
 
 # load the libQuasiDefinite
 include("../deps/deps.jl")
+
+#Generic LAPACK error handlers, copied from https://github.com/JuliaLang/julia/blob/master/base/linalg/lapack.jl
+macro assertargsok() #Handle only negative info codes - use only if positive info code is useful!
+    :(info[1]<0 && throw(ArgumentError("invalid argument #$(-info[1]) to LAPACK call")))
+end
+
+#Check that upper/lower (for special matrices) is correctly specified
+macro chkuplo()
+    :((uplo=='U' || uplo=='L') ||
+      throw(ArgumentError(string("uplo argument must be 'U' (upper) or 'L' (lower), got ", repr(uplo)))))
+end
+
+# from SCS.jl
+function __init__()
+    # default binaries need access to Julia's lapack symbols
+    @unix_only dlopen(Base.liblapack_name, RTLD_LAZY|RTLD_DEEPBIND|RTLD_GLOBAL)
+end
 
 # see: https://github.com/JuliaLang/julia/blob/master/base/linalg/lapack.jl#L1735
 @eval begin
     function qdtrf!(uplo::BlasChar, A::StridedMatrix{Float64})
         chkstride1(A)
         chksquare(A)
-        #@chkuplo
+        @chkuplo
         lda = max(1,stride(A,2))
         lda==0 && return A, 0
         info = Array(BlasInt, 1)
@@ -32,9 +48,11 @@ println("Hello from QuasiDefinite julia code.")
 
 ccall((:say_hello,libQuasiDefinite),Void,())
 
-A = [2.0 -1.0; -1.0 2.0]
-B, info = qdtrf!('U',A)
-show(B)
-show(info)
+function run()
+    A = Float64[2 -1; -1 2]
+    B, info = qdtrf!('U',A)
+    show(B)
+    show(info)
+end
 
 end # module
